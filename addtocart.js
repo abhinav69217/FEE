e903d280d1fdd2f8732e007d102c4d825d20fe2c
abhinav69217 <abhinav0045.be21@chitkara.edu.in>
2023-10-25 15:29:24 +0530

    finished with html css js

--- addtocart.js
+++ addtocart.js
@@ -92,42 +92,51 @@ document.getElementById("root").innerHTML = product
   })
   .join("");
 
-var cart = [];
-
+let cart = [];
 function addtocart(a) {
   cart.push({ ...product[a] });
   displaycart();
 }
+
+function displaycart() {
+  let j = 0;
+  let total = 0;
+  document.getElementById("count").innerHTML = cart.length;
+  let cartItemsHTML = "";
+
+  if (cart.length == 0) {
+    document.getElementById("cartItem").innerHTML = "Your cart is empty";
+    document.getElementById("total").innerHTML = "$ " + 0 + ".00";
+  } else {
+    cartItemsHTML = cart
+      .map((items) => {
+        var { image, title, price } = items;
+        total += price;
+        return `<div class='cart-item'>
+            <div class='row-img'>
+              <img class='rowimg' src=${image}>
+            </div>
+            <p style='font-size: 8px;'>${title}</p>
+            <h2 style='font-size: 10px;'>$ ${price}.00</h2>
+            <i class='fa-solid fa-trash' onclick='delElement(${j++})'></i>
+          </div>`;
+      })
+      .join("");
+  }
+  cartItemsHTML += "<button onclick='showPayment()'>BUY</button>";
+
+  document.getElementById("cartItem").innerHTML = cartItemsHTML;
+  document.getElementById("total").innerHTML = "$ " + total + ".00";
+}
+
 function delElement(a) {
   cart.splice(a, 1);
   displaycart();
 }
 
-function displaycart() {
-  let j = 0,
-    total = 0;
-  document.getElementById("count").innerHTML = cart.length;
-  if (cart.length == 0) {
-    document.getElementById("cartItem").innerHTML = "Your cart is empty";
-    document.getElementById("total").innerHTML = "$ " + 0 + ".00";
-  } else {
-    document.getElementById("cartItem").innerHTML = cart
-      .map((items) => {
-        var { image, title, price } = items;
-        total = total + price;
-        document.getElementById("total").innerHTML = "$ " + total + ".00";
-        return (
-          `<div class='cart-item'>
-                <div class='row-img'>
-                    <img class='rowimg' src=${image}>
-                </div>
-                <p style='font-size:8px;'>${title}</p>
-                <h2 style='font-size: 10px;'>$ ${price}.00</h2>` +
-          "<i class='fa-solid fa-trash' onclick='delElement(" +
-          j++ +
-          ")'></i></div>"
-        );
-      })
-      .join("");
-  }
+function showPayment() {
+  document.getElementById("payment").style.display = "block";
+}
+function closePayment() {
+  document.getElementById("payment").style.display = "none";
 }
